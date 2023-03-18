95ddb55fa4133f5c844a35b634ca24ed37954836
Pashs-ba <pashs.ba@gmail.com>
2023-03-18 17:25:16 +0300

    It works!

--- main.py
+++ main.py
@@ -1,75 +1,60 @@
-
-#Import modules for functions
+# Import modules for functions
 import logging
-
-#Import diffrent custom modules 
+import asyncio
 import toml
-import languageRU
+
+# Import diffrent custom modules
+import languageRU as RU
 import languageEN
 import languagePL
 import languageBY
 
-#Import modules for Aiogram
-from aiogram import Bot, Dispatcher, executor, types
-from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters.state import State, StatesGroup
-from aiogram.types.message import ContentType
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.dispatcher.filters import Text
-
+# Import modules for Aiogram
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
 
 with open('secrets.toml') as f:
-    key=toml.parse(f.read())
-
+    key = toml.loads(f.read())["key"]
 
 ########################### Bot configruration ################################
 ### Standart things for work
-#log
+# log
 logging.basicConfig(level=logging.INFO)
 
-#definicja pamięci
-starage = MemoryStorage()
+dp = Dispatcher()
 
-#init
-bot = Bot(token=config.TOKEN)
-dp = Dispatcher(bot, storage=starage)
-
-
+# init
+bot = Bot(token=key)
+dp = Dispatcher()
 
 
 ###################################################### Keyboards configruration ##################################################
 
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer(RU.RuStartPhrases)
 
 
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
 
 
-
-
+if __name__ == "__main__":
+    asyncio.run(main())
 
 
 ########################### Bot commands ################################
 ### Work with commands
 
 @dp.message_handler(commands=['start', 'help'])
-async def command_start(message : types.Message):
-    await bot.send_message(message.from_user.id, languageRU.RuFirstFraze, parse_mode="Markdown") 
-
-
-
-
-
-
-
-
-
-
+async def command_start(message: types.Message):
+    await bot.send_message(message.from_user.id, RU.RuFirstFraze, parse_mode="Markdown")
 
 
 ###################################################### Start bot ##################################################
 
-#run long polling
+# run long polling
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=False)
-
-
